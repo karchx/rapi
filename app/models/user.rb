78f6f664046233b63a9f69ce46b3342f362c6d87
Стив <kenethra.74@gmail.com>
2022-12-30 18:07:19 -0600

    feat: token generate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-
   has_many :posts
   validates :email, presence: true
   validates :name, presence: true
   validates :auth_token, presence: true
+
+  after_initialize :generate_auth_token
+
+  def generate_auth_token
+    # unless = if !
+    unless auth_token.present?
+      self.auth_token = TokenGenerationService.generate
+    end
+  end
 end
